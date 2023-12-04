b91ac4fc44ac7a1f4ac2bca9c3599f4a526a9192
Alfredo Castaldi <Alfredcastaldi1992@gmail.com>
2023-12-04 15:54:15 +0100

    prima modifica

---
+++ Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args){
+
+        System.out.println("ciao");
+        Persona pippo = new Persona("jack", 99);
+        System.out.println(pippo.getName());
+    }
+    
+}
---
+++ Persona.java
@@ -0,0 +1,15 @@
+public class Persona {
+    
+    private String name;
+    public int eta;
+
+     public Persona(String name, int eta){
+        this.name = name;
+        this.eta = eta;
+     }
+
+     public String getName() {
+         return name;
+     }
+
+}
